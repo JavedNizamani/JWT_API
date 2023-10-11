d0ffddb965c109e482337f7e850ae7b8528cd00f
Javed <javedahmednizamani@gmail.com>
2023-10-11 13:00:29 +0500

    simplified_Code

---
+++ controller/profile.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const secretKey = 'secretKey';
+
+var jwtData = (req, res)=>{
+    jwt.verify(req.token, secretKey,(err, authorizedData)=>{
+        if(err){
+            res.send({result: 'Invalid Token'});
+        }else{
+            res.json({message: "You are valid to access the profile", authorizedData});
+        }
+    })
+}
+
+function verifyToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    if(typeof authHeader !== 'undefined'){
+        const token = authHeader;
+        req.token = token;
+        next();
+    }else{
+        res.send({result: "Invalid Token"});
+    }
+}
+
+module.exports = {
+    verifyToken,
+    jwtData
+}
+
+
--- routes/profile.js
+++ routes/profile.js
@@ -2,26 +2,8 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const secretKey = 'secretKey';
 const router = express.Router();
+const jwtDataToken = require('../controller/profile');
 
-router.get('/profile',verifyToken, (req, res)=>{
-    jwt.verify(req.token, secretKey,(err, authorizedData)=>{
-        if(err){
-            res.send({result: 'Invalid Token'});
-        }else{
-            res.json({message: "You are valid to access the profile", authorizedData});
-        }
-    })
-});
-
-function verifyToken(req, res, next){
-    const authHeader = req.headers['authorization']
-    if(typeof authHeader !== 'undefined'){
-        const token = authHeader;
-        req.token = token;
-        next();
-    }else{
-        res.send({result: "Invalid Token"});
-    }
-}
+router.get('/profile',jwtDataToken.verifyToken,jwtDataToken.jwtData);
 
 module.exports = router;
